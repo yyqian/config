c657448ecc489fe60eb269dc3fe707fe150d6221
yyqian <yyqian>
2018-03-10 23:04:00 +0800

    auto-sync

--- scripts/ln.sh
+++ scripts/ln.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env sh
 SOURCE_DIR="$HOME/work/config"
 
+link () {
+  rm $TARGET_DIR/$FILE
+  ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
+}
+
+sudolink () {
+  sudo rm $TARGET_DIR/$FILE
+  sudo ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
+}
+
 FILE="settings.json"
 TARGET_DIR="$HOME/.config/Code/User"
-rm $TARGET_DIR/$FILE
-ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
 
 FILE=".gitconfig"
 TARGET_DIR="$HOME"
-rm $TARGET_DIR/$FILE
-ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
+link
 
 FILE=".vimrc"
 TARGET_DIR="$HOME"
-rm $TARGET_DIR/$FILE
-ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
+link
 
 FILE=".zshrc"
 TARGET_DIR="$HOME"
-rm $TARGET_DIR/$FILE
-ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
+link
 
 FILE="sources.list"
 TARGET_DIR="/etc/apt"
-sudo rm $TARGET_DIR/$FILE
-sudo ln -s $SOURCE_DIR/$FILE $TARGET_DIR/$FILE
+sudolink
